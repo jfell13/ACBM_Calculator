0ca025a96327992a86cda62d9b2c41ede23d27c3
Jason Fell <jsfell@ucdavis.edu>
2020-05-11 18:14:27 -0700

    Add files via upload

--- application.py
+++ application.py
@@ -2,12 +2,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-#import matplotlib.plot as plt
 import numpy as np
 import plotly.graph_objs as go
-import plotly.express as px
+#import plotly.express as px
 import os
-#import pandas as pd
 import base64
 import webbrowser
 from threading import Timer
